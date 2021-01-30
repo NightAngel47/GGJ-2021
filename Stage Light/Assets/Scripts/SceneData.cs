4f384936729c0a6a9b4dd9e47aaec28351891015
Steven <NightAngel47@users.noreply.github.com>
2021-01-30 15:03:17 -0600

    WIP on Setting Light Positions

--- Stage Light/Assets/Scripts/SceneData.cs
+++ Stage Light/Assets/Scripts/SceneData.cs
@@ -11,9 +11,9 @@ public class SceneData : ScriptableObject
     public List<int> LightIndexes => lightIndexes;
     public List<Vector3> Positions => new List<Vector3>()
     {
-        new Vector3(-4.5f, 3f, 0f), new Vector3(0, 3f, 0f), new Vector3(4.5f, 3f, 0f),
-        new Vector3(-4.5f, 0.63f, 0f), new Vector3(0, 0.63f, 0f), new Vector3(4.5f, 0.63f, 0f),
         new Vector3(-4.5f, -1.6f, 0f), new Vector3(0, -1.6f, 0f), new Vector3(4.5f, -1.6f, 0f),
+        new Vector3(-4.5f, 0.63f, 0f), new Vector3(0, 0.63f, 0f), new Vector3(4.5f, 0.63f, 0f),
+        new Vector3(-4.5f, 3f, 0f), new Vector3(0, 3f, 0f), new Vector3(4.5f, 3f, 0f)
     };
     public List<StagePoint> RequestsForScene => requestsForScene;
 }
