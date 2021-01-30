727c29b525e082c75e6ed6bc39606965a3c5e168
Alex Cline <clinealex10@gmail.com>
2021-01-30 12:10:58 -0600

    ActorStuff

--- Stage Light/Assets/Scripts/ActorBehavior.cs
+++ Stage Light/Assets/Scripts/ActorBehavior.cs
@@ -1,8 +1,50 @@
+using NaughtyAttributes;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ActorBehavior : MonoBehaviour
 {
+    [SerializeField] private float speed = 1f;
     [SerializeField] private StagePoint request;
+
+    private Vector3 offstagePoint = Vector3.zero;
+    private Vector3 startPoint = Vector3.zero;
+    private Vector3 targetPoint = Vector3.zero;
+    private bool isMoving = false;
+    private float lerpStep = 0f;
+
+    private void Awake()
+    {
+        offstagePoint = transform.position;
+    }
+
+    private void Update()
+    {
+        if (isMoving)
+        {
+            transform.position = Vector3.Lerp(startPoint, targetPoint, lerpStep);
+            lerpStep += Time.deltaTime * speed;
+            if (transform.position == targetPoint)
+                isMoving = false;
+        }
+    }
+
+    [Button("On Stage")]
+    public void MoveOnStage()
+    {
+        lerpStep = 0f;
+        startPoint = offstagePoint;
+        targetPoint = PuzzleManager.Instance.CurrentSceneData.Positions[request.positionIndex];
+        isMoving = true;
+    }
+
+    [Button("Off Stage")]
+    public void MoveOffStage()
+    {
+        lerpStep = 0f;
+        startPoint = PuzzleManager.Instance.CurrentSceneData.Positions[request.positionIndex];
+        targetPoint = offstagePoint;
+        isMoving = true;
+    }
 }
