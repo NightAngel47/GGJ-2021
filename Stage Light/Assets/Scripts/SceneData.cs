b5fa98fd83773f64159bb631ac7a64484766f4f5
Alex Cline <clinealex10@gmail.com>
2021-01-30 14:30:35 -0600

    Positions are now a static property

--- Stage Light/Assets/Scripts/SceneData.cs
+++ Stage Light/Assets/Scripts/SceneData.cs
@@ -6,10 +6,14 @@ using UnityEngine;
 public class SceneData : ScriptableObject
 {
     [SerializeField] private List<int> lightIndexes;
-    [SerializeField] private List<Vector3> positions;
     [SerializeField] private List<StagePoint> requestsForScene;
 
     public List<int> LightIndexes => lightIndexes;
-    public List<Vector3> Positions => positions;
+    public List<Vector3> Positions => new List<Vector3>()
+    {
+        new Vector3(-4.5f, 3f, 0f), new Vector3(0, 3f, 0f), new Vector3(4.5f, 3f, 0f),
+        new Vector3(-4.5f, 0.63f, 0f), new Vector3(0, 0.63f, 0f), new Vector3(4.5f, 0.63f, 0f),
+        new Vector3(-4.5f, -1.6f, 0f), new Vector3(0, -1.6f, 0f), new Vector3(4.5f, -1.6f, 0f),
+    };
     public List<StagePoint> RequestsForScene => requestsForScene;
 }
