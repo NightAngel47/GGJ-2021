af984446ce67ecfafc2e6ab969dff3887adc4a03
Alex Cline <clinealex10@gmail.com>
2021-01-30 16:00:09 -0600

    idk

--- Stage Light/Assets/Scripts/PuzzleManager.cs
+++ Stage Light/Assets/Scripts/PuzzleManager.cs
@@ -53,6 +53,11 @@ public class PuzzleManager : MonoBehaviour
     {
         SelectedFixtureChanged?.Invoke(CurrentSceneData.LightIndexes[indexOfDataList]);
     }
+
+    public void StartScene()
+    {
+
+    }
 }
 
 [Serializable]
