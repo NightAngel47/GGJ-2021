4a1664731d2620eaf1417dd3cb684180b218abbc
Steven <NightAngel47@users.noreply.github.com>
2021-01-30 23:03:42 -0600

    Updated Positions?

--- Stage Light/Assets/Scripts/SceneData.cs
+++ Stage Light/Assets/Scripts/SceneData.cs
@@ -6,19 +6,15 @@ using UnityEngine;
 public class SceneData : ScriptableObject
 {
     public List<Color> colors;
+    public List<Vector3> positions = new List<Vector3>();
     [SerializeField] private List<int> lightIndexes;
     [SerializeField] private List<StagePoint> requestsForScene;
 
-    public List<Color> Colors => Colors;
+    public List<Color> Colors => colors;
+    public List<Vector3> Positions => positions;
     
     public List<int> LightIndexes => lightIndexes;
 
-    public List<Vector3> Positions => new List<Vector3>()
-    {
-        new Vector3(-4.5f, -1.6f, 0f), new Vector3(0, -1.6f, 0f), new Vector3(4.5f, -1.6f, 0f),
-        new Vector3(-4.5f, 0.63f, 0f), new Vector3(0, 0.63f, 0f), new Vector3(4.5f, 0.63f, 0f),
-        new Vector3(-4.5f, 3f, 0f), new Vector3(0, 3f, 0f), new Vector3(4.5f, 3f, 0f)
-    };
 
     public List<StagePoint> RequestsForScene => requestsForScene;
 }
