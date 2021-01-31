469aebdb79d26d133b75e94b64d1521dc155b49c
Steven <NightAngel47@users.noreply.github.com>
2021-01-30 21:39:00 -0600

    Color Palette

--- Stage Light/Assets/Scripts/SceneData.cs
+++ Stage Light/Assets/Scripts/SceneData.cs
@@ -5,15 +5,20 @@ using UnityEngine;
 [Serializable, CreateAssetMenu(fileName = "New SceneData", menuName = "SceneData")]
 public class SceneData : ScriptableObject
 {
+    public List<Color> colors;
     [SerializeField] private List<int> lightIndexes;
     [SerializeField] private List<StagePoint> requestsForScene;
 
+    public List<Color> Colors => Colors;
+    
     public List<int> LightIndexes => lightIndexes;
+
     public List<Vector3> Positions => new List<Vector3>()
     {
         new Vector3(-4.5f, -1.6f, 0f), new Vector3(0, -1.6f, 0f), new Vector3(4.5f, -1.6f, 0f),
         new Vector3(-4.5f, 0.63f, 0f), new Vector3(0, 0.63f, 0f), new Vector3(4.5f, 0.63f, 0f),
         new Vector3(-4.5f, 3f, 0f), new Vector3(0, 3f, 0f), new Vector3(4.5f, 3f, 0f)
     };
+
     public List<StagePoint> RequestsForScene => requestsForScene;
 }
